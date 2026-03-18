feaa2f8dd40617886941ec3d0d63252a752bd249
Tim Narr <hallo@tim-narr.com>
2026-03-18 21:57:09 +0100

    chore: adjust test to use required 'crop' => true option

--- tests/srcsetTest.php
+++ tests/srcsetTest.php
@@ -12,7 +12,7 @@ class SrcsetTest extends TestCase
 	{
 		$srcset = [
 			'test-preset' => [
-				'100w'  => ['width' => 100, 'crop' => false, 'quality' => 100, 'sharpen' => 10],
+				'100w'  => ['width' => 100, 'crop' => true, 'quality' => 100, 'sharpen' => 10],
 				'200w'  => ['width' => 200, 'crop' => true, 'quality' => 80],
 				'300w'  => ['width' => 300, 'crop' => true],
 				'400w'  => ['width' => 400],
@@ -24,7 +24,7 @@ class SrcsetTest extends TestCase
 
 		$expected = [
 			'test-preset' => [
-				'100w'  => ['width' => 100, 'height' => 56,  'crop' => false, 'quality' => 100, 'sharpen' => 10], // 56.25
+				'100w'  => ['width' => 100, 'height' => 56,  'crop' => true, 'quality' => 100, 'sharpen' => 10], // 56.25
 				'200w'  => ['width' => 200, 'height' => 113, 'crop' => true, 'quality' => 80], // 112.5
 				'300w'  => ['width' => 300, 'height' => 169, 'crop' => true], // 168.75
 				'400w'  => ['width' => 400, 'height' => 225],
@@ -40,7 +40,7 @@ class SrcsetTest extends TestCase
 	{
 		$srcset = [
 			'test-preset' => [
-				'100w'  => ['width' => 100, 'crop' => false, 'quality' => 100, 'sharpen' => 10],
+				'100w'  => ['width' => 100, 'crop' => true, 'quality' => 100, 'sharpen' => 10],
 				'200w'  => ['width' => 200, 'crop' => true, 'quality' => 80],
 				'300w'  => ['width' => 300, 'crop' => true],
 			],
@@ -51,7 +51,7 @@ class SrcsetTest extends TestCase
 
 		$expected = [
 			'test-preset' => [
-				'100w'  => ['width' => 100, 'height' => 67,  'crop' => false, 'quality' => 100, 'sharpen' => 10], // 66.67
+				'100w'  => ['width' => 100, 'height' => 67,  'crop' => true, 'quality' => 100, 'sharpen' => 10], // 66.67
 				'200w'  => ['width' => 200, 'height' => 133, 'crop' => true, 'quality' => 80], // 133.33
 				'300w'  => ['width' => 300, 'height' => 200, 'crop' => true],
 			],
